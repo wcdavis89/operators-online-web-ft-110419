febd64be613d7425bdd9623f1e22f4481ae6d769
Whittney Davis <wcdavis89@yahoo.com>
2019-11-08 04:21:25 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,18 @@
 def unsafe?(speed)
-speed>60 ? true: false
+if speed>60 
+  puts true
+  elsif speed<40
+  puts true
+else
+  false
+end
 
 end
 
 
 
 def not_safe?(speed)
-	
+	speed>60 ? true: false
 end
 	
 
